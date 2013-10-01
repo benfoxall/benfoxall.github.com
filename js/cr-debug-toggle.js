fb50150167b2a105ebc67d22cdf15637f1014492
Ben Foxall <benfoxall@gmail.com>
2013-10-01 22:08:59 +0100

    added new cr and cr-debug scripts

---
+++ js/cr-debug-toggle.js
@@ -0,0 +1,11 @@
+define([], function(){
+	var isDebug = window.location.search === '?cr-debug';
+	return function(element){
+    var a = document.createElement('a');
+    a.className = 'btn btn-primary btn-lg';
+		a.href = '';
+		a.search = isDebug ? '' : '?cr-debug';
+		a.innerHTML = isDebug ? 'hide loading' : 'show script loading';
+    element.appendChild(a);
+	}
+})
