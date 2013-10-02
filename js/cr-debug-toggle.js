9b14d46be00ee770b31b0ef01bd46f3ab223aa79
Ben Foxall <benfoxall@gmail.com>
2013-10-02 16:49:03 +0100

    switch back to index for demo
    (much cooler)

--- js/cr-debug-toggle.js
+++ js/cr-debug-toggle.js
@@ -3,7 +3,7 @@ define([], function(){
 	return function(element){
     var a = document.createElement('a');
     a.className = 'btn btn-primary btn-lg';
-		a.href = '';
+		a.href = isDebug ? '' : '/';
 		a.search = isDebug ? '' : '?cr-debug';
 		a.innerHTML = isDebug ? 'hide loading' : 'show script loading';
     element.appendChild(a);
