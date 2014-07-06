48372d9bde40e76dcd992dfd78025ed430a5c4dc
Ben Foxall <benfoxall@gmail.com>
2014-07-06 15:07:40 +0100

    post content

--- js/email-stats.js
+++ js/email-stats.js
@@ -1,10 +1,12 @@
 define(['d3'], function(d3){
 
-
 	return function(element){
-		console.log("..", element)
+		var stat = element.getAttribute('data-stat');
+
+		console.log("..", stat)
 		var key = 'inbox-work';
-		var root = 'http://email-stats.herokuapp.com/'
+		var root = 'http://email-stats.herokuapp.com'
+		// var root = 'http://localhost:3000'
 
 
 
@@ -49,37 +51,79 @@ define(['d3'], function(d3){
 		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-			d3.json(root + '/data/' + key, function(error, data) {
-				if (error) return console.warn(error);
+		var dataUnread = [],
+			dataRead   = [];
 
-
-				
-				data.forEach(function(d) {
-					d.time = new Date(d[0]);
-					d.count = d[1];
-				});
-
-			  x.domain(d3.extent(data, function(d) { return d.time; }));
-			  y.domain([0,d3.max(data, function(d) { return d.count; })]);
-
-			  svg.append("g")
+		var x_ = svg.append("g")
 			      .attr("class", "x axis")
-			      .attr("transform", "translate(0," + height + ")")
-			      .call(xAxis);
+			      .attr("transform", "translate(0," + height + ")");
 
-			  svg.append("g")
+		var y_ = svg.append("g")
 			      .attr("class", "y axis")
-			      .attr("transform", "translate(" + width + ", 0)")
-			      .call(yAxis)
+			      .attr("transform", "translate(" + width + ", 0)");
 
-			  svg.append("path")
-			      .datum(data)
+		var unread_ = svg.append("path")
 			      .attr("class", "area")
+			      .style("fill", 'rgb(255, 235, 0)')
+
+		var read_ = svg.append("path")
+			      .attr("class", "area")
+			      .style("fill", 'rgb(255, 153, 0)')
+
+		function render(){
+			var allData = dataRead.concat(dataUnread);
+
+			data = dataUnread;
+
+			  x.domain(d3.extent(allData, function(d) { return d.time; }));
+			  y.domain([0,d3.max(allData, function(d) { return d.count; })]);
+
+			  
+			  x_    .call(xAxis);
+
+			  y_    .call(yAxis)
+
+			  read_
+			      .datum(data)
+			      .attr("d", area);
+
+			  unread_
+			      .datum(dataRead)
 			      .attr("d", area);
 
 
 
+		}
+
+
+		d3.json(root + '/data/inbox-' + stat, function(error, data) {
+			if (error) return console.warn(error);
+
+			data.forEach(function(d) {
+				d.time = new Date(d[0]);
+				d.count = d[1];
 			});
+
+			dataRead = data;
+
+			render();
+
+		});
+
+
+		d3.json(root + '/data/inbox-' + stat + '-unread', function(error, data) {
+			if (error) return console.warn(error);
+
+			data.forEach(function(d) {
+				d.time = new Date(d[0]);
+				d.count = d[1];
+			});
+
+			dataUnread = data;
+
+			render();
+
+		});
 
 
 
