c0a3006c5bab4cb603f50c54d7f3d7f9a28edd7c
Ben Foxall <benfoxall@gmail.com>
2016-12-20 00:38:28 +0000

    I forget even what

--- js/sound-frequency-graph.js
+++ js/sound-frequency-graph.js
@@ -1,8 +1,47 @@
 define(['sound-audio-ctx'], function(audioCtx){
 
+  var oscillator = audioCtx.createOscillator()
+  oscillator.type = 'sine'
+  oscillator.frequency.value = 15000
+
+  oscillator.start()
+  oscillator.stop(audioCtx.currentTime + 5)
+
+  var analyser = audioCtx.createAnalyser()
+  var bufferLength = analyser.frequencyBinCount
+  var data = new Uint8Array(bufferLength)
+  // analyser.getByteTimeDomainData(data)
+
+  oscillator.connect(analyser)
+
   return function(canvas){
     var ctx = canvas.getContext('2d')
-    ctx.fillRect(0,0,30,30)
+
+    var w = canvas.width,
+        h = canvas.height
+
+    var c = 100
+    var interval = setInterval(function(){
+        analyser.getByteFrequencyData(data)
+
+        var s = w/data.length
+        var s2 = h/255
+
+        ctx.clearRect(0,0, w, h)
+        ctx.beginPath()
+        for (var i = 0; i < data.length; i++) {
+          ctx.lineTo(i * s, data[i] * s2)
+        }
+        ctx.stroke()
+
+        if(c-- < 0) {
+          clearInterval(interval)
+        }
+    },10)
+
+
+
+
 
   }
 })
