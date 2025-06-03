c0a12fc380223d9484c79fc37317ceae69d2d1a4
Ben Foxall <benfoxall@gmail.com>
2025-06-03 11:14:24 +0100

    Add content test

---
+++ test/archive.compare.js
@@ -0,0 +1,45 @@
+import assert from 'node:assert'
+import { readdir, readFile } from 'node:fs/promises'
+import test from 'node:test'
+import { join } from 'path'
+import { JSDOM } from 'jsdom'
+
+// tar -xzf archive.tar.gz
+const reference = join(import.meta.dirname, '_site')
+const target = join(import.meta.dirname, '../_site')
+
+
+for await (const file of walk(reference)) {
+
+  test(file, async () => {
+
+    const a = await readFile(join(reference, file))
+    const b = await readFile(join(target, file))
+
+    if (file.endsWith('.html')) {
+
+      const domA = new JSDOM(a)
+      const domB = new JSDOM(b)
+
+      assert.equal(domA.window.document.title, domB.window.document.title, "Title matches")
+
+    } else {
+      assert.ok(a.equals(b), "content matches")
+    }
+  })
+
+}
+
+
+async function* walk(base, dir = '') {
+  for (const entry of await readdir(join(base, dir), { withFileTypes: true })) {
+    const path = join(dir, entry.name)
+
+    if (entry.isDirectory())
+      yield* walk(base, path)
+
+    else if (entry.isFile())
+      yield path
+
+  }
+}
